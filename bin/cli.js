174768127619021f0c5e26ed6d01aeabef28fed5
skepticfx <ahamed.nafeez@gmail.com>
2014-06-06 00:58:30 +0530

    cli: add forwarding option

--- bin/cli.js
+++ bin/cli.js
@@ -9,6 +9,7 @@ program
   .version('0.1.1')
   .option('-l, --listen <port>', 'TLS port to listen on. Defaults to 443', parseInt)
   .option('-w, --logfile <filename>', 'Log requests to a file', String)
+  .option('-f, --forward <filename>', 'Forward all requests to a specific host', String)
   .parse(process.argv);
 
 program.outputHelp();
@@ -19,18 +20,40 @@ var tlsOptions = {
   ciphers: 'ALL:!LOW:!DSS:!EXP'
 };
 
-tlsjack = tlsjack.listen(program.listen || 443)
-                 .logInfo()
-                 .logRequest();
+tlsjack =
+  tlsjack
+    .listen(program.listen || 443)
+    .logInfo()
+    .logRequest()
+    .logResponse();
 
 if(program.logfile){
   tlsjack =
     tlsjack
       .logRequest()
+      .logResponse()
       .writeLogs(program.logfile || "")
-      // Disable logger, to stop console.log
-      .setLogger(function(data){ return data;});
+      // Disable Non-Printable characters while logging, to stop console.log
+      .setLogger(function(data){
+        print_safe(data);
+        return data;
+      });
 }
 
-
+if(program.forward){
+  var fw = program.forward || 'localhost';
+  tlsjack = tlsjack.forward(fw);
+  console.log('Forwarding all requests to ' + fw);
+}
 tlsjack = tlsjack.start(tlsOptions);
+
+
+function print_safe(data){
+  var x;
+  for(x=0; x < data.lenght; x++){
+    var charCode = data.charCodeAt(x);
+    if( charCode >= 32 && charCode <= 126){
+      console.log(data[x]);
+    }
+  }
+}
